0fd0c99e13c67a20563a37558a8fa970c2ca013d
CobraBite-096 <sarveshsrinivas360@gmail.com>
2025-11-12 13:13:59 +0530

    first iteration of integration - floor plan UI and actual floor plan. improved selection logic.

--- bundle.js
+++ bundle.js
@@ -1,5 +1,45 @@
 var SELECTED_ROOMS = [];
 var classes = document.getElementsByClassName("nav-button");
+var floorSelection = document.getElementById('floorSelection');
+var floorPlanView = document.getElementById('floorPlanView');
+var messageBox = document.getElementById('messageBox');
+function showFeedback(message, type) {
+    if (type === void 0) { type = 'success'; }
+    messageBox.textContent = message;
+    if (type === 'error') {
+        messageBox.style.backgroundColor = '#ef4444';
+    }
+    else {
+        messageBox.style.backgroundColor = '#1D4ED8';
+    }
+    messageBox.classList.add('show');
+    setTimeout(function () {
+        messageBox.classList.remove('show');
+    }, 3000);
+}
+function selectFloor(floorName) {
+    showFeedback("Navigating to ".concat(floorName, " Plan..."));
+    floorSelection.classList.add('hidden');
+    floorPlanView.classList.remove('hidden');
+}
+function showAreaFeedback(areaName, deselect) {
+    if (deselect === void 0) { deselect = false; }
+    var showText = "Selected Area: ".concat(areaName);
+    if (deselect) {
+        showText = "De-".concat(showText);
+    }
+    showFeedback(showText, 'success');
+    console.log("User clicked on: ".concat(areaName));
+}
+function goBackToSelection() {
+    floorPlanView.classList.add('hidden');
+    floorSelection.classList.remove('hidden');
+}
+document.getElementById("secondFloor").addEventListener("click", function () { showFeedback("Second Floor has not been implemented yet"); });
+document.getElementById("backToFloorSelect").addEventListener("click", function () {
+    goBackToSelection();
+});
+document.getElementById("firstFloor").addEventListener("click", function () { selectFloor("First Floor"); });
 var _loop_1 = function (ii) {
     var ele = classes[ii];
     ele.addEventListener("click", function () {
@@ -7,80 +47,35 @@ var _loop_1 = function (ii) {
         console.log(roomId, ele.id);
         roomId = roomId.replace("btn", "");
         roomId = roomId[0].toUpperCase().concat(roomId.slice(1));
+        if (roomId.split(" ").length > 1) {
+            roomId = "".concat(roomId.split(" ")[0]).concat(" ").concat(roomId.split(" ")[1][0].toUpperCase().concat(roomId.split(" ")[1].slice(1)));
+        }
         console.log(SELECTED_ROOMS);
         if (SELECTED_ROOMS.indexOf(roomId) != -1) {
             SELECTED_ROOMS.splice(SELECTED_ROOMS.indexOf(roomId), 1);
+            ele.style.background = "rgba(255, 255, 255, 0.95)";
+            showAreaFeedback(roomId, true);
         }
         else {
             SELECTED_ROOMS.push(roomId);
+            ele.style.background = "rgba(129, 231, 245, 0.95)";
+            showAreaFeedback(roomId);
         }
-        var stringBuild = "";
-        SELECTED_ROOMS.forEach(function (room) { return stringBuild = stringBuild.concat(room.concat(", ")); });
-        if (stringBuild.length > 4)
-            stringBuild = stringBuild.slice(0, stringBuild.length - 2);
-        else
-            stringBuild = "None";
-        document.getElementById("selected_rooms").innerText = "Selected Rooms: ".concat(stringBuild);
+        var refresh = document.getElementById("selected_rooms").children;
+        for (var j = 0; j < refresh.length; j++) {
+            refresh[j].style.display = "none";
+        }
+        for (var jj = 0; jj < SELECTED_ROOMS.length; jj++) {
+            if (!SELECTED_ROOMS[jj])
+                continue;
+            var element = document.createElement("div");
+            element.setAttribute("id", "selectedRoom-" + SELECTED_ROOMS[jj]);
+            element.classList.add("roomSelect");
+            element.innerText = SELECTED_ROOMS[jj];
+            document.getElementById("selected_rooms").appendChild(element);
+        }
     });
 };
 for (var ii = 0; ii < classes.length; ii++) {
     _loop_1(ii);
 }
-var _a, _b;
-if (document.readyState == "complete") {
-    document.getElementById("hours").onkeydown = function () {
-        if (12 < Number(document.getElementById("hours").value) || Number(document.getElementById("hours").value) < -1) {
-            document.getElementById("hours").value = "12";
-        }
-    };
-    document.getElementById("mins").onkeydown = function () {
-        if (59 < Number(document.getElementById("mins").value) || Number(document.getElementById("hours").value) < -1) {
-            document.getElementById("mins").value = "30";
-        }
-    };
-}
-function showFeedback(message) {
-    var box = document.getElementById('messageBox');
-    box.textContent = message;
-    box.className = 'message-box show p-3 rounded-lg shadow-xl bg-nest-blue text-white';
-    setTimeout(function () {
-        box.classList.remove('show');
-    }, 3000);
-}
-function selectFloor(floorName) {
-    console.log("User selected: ".concat(floorName));
-    showFeedback("Navigating to ".concat(floorName, " Dashboard..."));
-}
-(_a = document.getElementById("firstFloor")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
-    selectFloor("First Floor");
-});
-(_b = document.getElementById("secondFloor")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", function () {
-    selectFloor("Second Floor");
-});
-function login_showFeedback(message, type) {
-    if (type === void 0) { type = 'success'; }
-    var box = document.getElementById('messageBox');
-    box.textContent = message;
-    box.className = 'message-box show p-3 rounded-lg shadow-xl';
-    if (type === 'success') {
-        box.classList.add('bg-green-500', 'text-white');
-    }
-    else {
-        box.classList.add('bg-red-500', 'text-white');
-    }
-    setTimeout(function () {
-        box.classList.remove('show');
-    }, 3000);
-}
-function handleLogin(event) {
-    event.preventDefault();
-    var name = document.getElementById('name').value;
-    console.log("Attempting login...");
-    console.log("Name:", name);
-    login_showFeedback("Access Granted. Welcome, ".concat(name, "! Redirecting..."), 'success');
-    setTimeout(function () {
-        window.location.href = 'floor_selection.html';
-    }, 1000);
-    return false;
-}
-//# sourceMappingURL=bundle.js.map
---
+++ integratedV1.ts
@@ -0,0 +1,81 @@
+let SELECTED_ROOMS: Array<string> = []
+const classes = document.getElementsByClassName("nav-button")
+const floorSelection = document.getElementById('floorSelection');
+const floorPlanView = document.getElementById('floorPlanView');
+const messageBox = document.getElementById('messageBox');
+
+
+function showFeedback(message: string, type = 'success') {
+    messageBox.textContent = message;
+
+    if (type === 'error') {
+        messageBox.style.backgroundColor = '#ef4444';
+    } else {
+        messageBox.style.backgroundColor = '#1D4ED8';
+    }
+
+    messageBox.classList.add('show');
+
+    setTimeout(() => {
+        messageBox.classList.remove('show');
+    }, 3000);
+}
+
+function selectFloor(floorName: string) {
+    showFeedback(`Navigating to ${floorName} Plan...`);
+    floorSelection.classList.add('hidden');
+    floorPlanView.classList.remove('hidden');
+}
+
+function showAreaFeedback(areaName: string, deselect=false) {
+    let showText=`Selected Area: ${areaName}`
+    if (deselect){showText = "De-".concat(showText)}
+    showFeedback(showText, 'success');
+    console.log(`User clicked on: ${areaName}`);
+}
+
+function goBackToSelection() {
+    floorPlanView.classList.add('hidden');
+    floorSelection.classList.remove('hidden');
+}
+
+document.getElementById("secondFloor").addEventListener("click", () => {showFeedback("Second Floor has not been implemented yet")})
+document.getElementById("backToFloorSelect").addEventListener("click", () => {
+    goBackToSelection()
+})
+document.getElementById("firstFloor").addEventListener("click", () => {selectFloor("First Floor")})
+for (let ii=0; ii <classes!.length; ii++){
+    const ele = <HTMLElement>classes[ii]!;
+    ele.addEventListener("click", ()=>{
+        let roomId = (ele.id).replace("-", "").replace("-", " ").replace("-", " ")
+        console.log(roomId, ele.id)
+        roomId = roomId.replace("btn", "")
+        roomId= roomId[0].toUpperCase().concat(roomId.slice(1))
+        if (roomId.split(" ").length > 1){
+            roomId = "".concat(roomId.split(" ")[0]).concat(" ").concat(roomId.split(" ")[1][0].toUpperCase().concat(roomId.split(" ")[1].slice(1)))
+        }
+        console.log(SELECTED_ROOMS)
+        if (SELECTED_ROOMS.indexOf(roomId) != -1){
+            SELECTED_ROOMS.splice(SELECTED_ROOMS.indexOf(roomId), 1)
+            ele.style.background= "rgba(255, 255, 255, 0.95)"
+            showAreaFeedback(roomId, true)
+        }else{
+            SELECTED_ROOMS.push(roomId)
+            ele.style.background = "rgba(129, 231, 245, 0.95)"
+            showAreaFeedback(roomId)
+        }
+        const refresh = document.getElementById("selected_rooms").children
+        for (let j=0; j < refresh.length; j++){
+            (<HTMLElement>refresh[j]).style.display = "none"
+                }
+        for (let jj=0;jj<SELECTED_ROOMS.length; jj++){
+            if (! SELECTED_ROOMS[jj]) continue;
+            const element = document.createElement("div")
+            element.setAttribute("id", "selectedRoom-"+SELECTED_ROOMS[jj])
+            element.classList.add("roomSelect")
+            element.innerText= SELECTED_ROOMS[jj]
+            document.getElementById("selected_rooms").appendChild(element)
+        }
+    })
+}
+
